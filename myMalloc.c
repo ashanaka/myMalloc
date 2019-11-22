898db5a5ff31093ad440cab26930a7af3b41c13f
Ashan Shanaka <jpashanshanaka@gmail.com>
2019-11-22 15:58:30 +0530

    myMalloc function is created

--- myMalloc.c
+++ myMalloc.c
@@ -17,6 +17,7 @@ struct metaData* freeBlock = (void*)memory;
 
 //Initiate the first metaData block
 void initiate(){
+	
 	freeBlock->sizeOfChunk  = 25000-sizeof(struct metaData);
 	freeBlock->isFree = 1;
 	freeBlock->next = NULL;
@@ -25,6 +26,7 @@ void initiate(){
 
 //Split function for allocating a first-fit memory block between two allocated memory blocks
 void split(struct metaData* firstFitSlot, size_t sizeOfChunk){
+	
 	struct metaData* newBlock = (void*)(firstFitSlot + sizeOfChunk + sizeof(struct metaData));
 	
 	newBlock->isFree = 1;
@@ -34,6 +36,41 @@ void split(struct metaData* firstFitSlot, size_t sizeOfChunk){
 	firstFitSlot->next = newBlock;
 	firstFitSlot->sizeOfChunk = sizeOfChunk;
 	firstFitSlot->isFree = 0;
+}
+
+
+//my malloc function to allocate memory dynamically in virtual memory
+void* myMalloc(size_t sizeOfBytes){
+	
+	struct metaData* thisBlock;
+	struct metaData* prevBlock;
+	
+	void* memoryAddress;
+	
+	if(!(freeBlock->sizeOfChunk)){
+		initiate();
+		printf("Memory Initiated!\n");
+	}	
+	
+	thisBlock = freeBlock;
+	
+	while((thisBlock->sizeOfChunk < sizeOfBytes || thisBlock->isFree == 0) && thisBlock->next != NULL){
+		
+		prevBlock = thisBlock;
+		thisBlock = thisBlock->next;
+		printf("One block is checked!\n");
+	}
+	
+	if(thisBlock->sizeOfChunk == sizeOfBytes){	//when the required size is absolutely fit into the selected block;
+		
+		memoryAddress = ++thisBlock;
+		thisBlock->isFree = 0;
+		printf("Allocated the absolute fitting chunk!\n");
+		return thisBlock;
+	}else if(thisBlock->sizeOfChunk > (sizeOfBytes + sizeof(struct metaData))){
+		
+		split(thisBlock, sizeOfBytes);
+	}
 }
 
 
