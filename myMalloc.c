6ca3a0991c5013f6812f8f53537793ed0884b765
Ashan Shanaka <jpashanshanaka@gmail.com>
2019-11-22 08:39:56 +0530

    split function created

--- myMalloc.c
+++ myMalloc.c
@@ -3,7 +3,7 @@
 
 char memory[25000];
 
-
+//The structure of the meta-data block to have all the data of the memory chunk to be allocated
 struct metaData{
 	size_t sizeOfChunk;
 	int isFree;
@@ -14,11 +14,26 @@ struct metaData{
 //Creating the pointer variable to get free memory blocks
 struct metaData* freeBlock = (void*)memory;
 
+
 //Initiate the first metaData block
 void initiate(){
 	freeBlock->sizeOfChunk  = 25000-sizeof(struct metaData);
 	freeBlock->isFree = 1;
 	freeBlock->next = NULL;
+}
+
+
+//Split function for allocating a first-fit memory block between two allocated memory blocks
+void split(struct metaData* firstFitSlot, size_t sizeOfChunk){
+	struct metaData* newBlock = (void*)(firstFitSlot + sizeOfChunk + sizeof(struct metaData));
+	
+	newBlock->isFree = 1;
+	newBlock->sizeOfChunk = firstFitSlot->sizeOfChunk - sizeOfChunk - sizeof(struct metaData);
+	newBlock->next = firstFitSlot->next;
+	
+	firstFitSlot->next = newBlock;
+	firstFitSlot->sizeOfChunk = sizeOfChunk;
+	firstFitSlot->isFree = 0;
 }
 
 
