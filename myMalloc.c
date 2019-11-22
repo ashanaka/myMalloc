3b869ac5a23bd6391328f85b24928ea34397e11e
Ashan Shanaka <ashansjp@gmail.com>
2019-11-22 08:03:18 +0530

    Create myMalloc.c

---
+++ myMalloc.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stddef.h>
+
+char memory[25000];
+
+
+struct metaData{
+	size_t sizeOfChunk;
+	int isFree;
+	struct block *next;
+};
+
+
+//Creating the 
+struct metaData* freeBlock = (void*)memory;
+
+//Initiate the first metaData block
+void initiate(){
+	freeBlock->sizeOfChunk  = 25000-sizeof(struct metaData);
+	freeBlock->isFree = 1;
+	freeBlock->next = NULL;
+}
+
+
+//For testing purposes only
+int main(){
+	
+	struct metaData *block = (void*)memory;
+	block->isFree = 1;
+	block->next = NULL;
+	block->sizeOfChunk = sizeof(struct metaData);
+	
+	printf("%d", (block->sizeOfChunk));
+	
+	return 0;
+}
